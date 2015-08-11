244d8802c143b9080ebf40d43da5af02d45c3df5
admin <admin@wangqiangdeMacBook-Pro-2.local>
2015-08-11 17:26:37 +0800

    add3

--- Bezier.py
+++ Bezier.py
@@ -2,28 +2,22 @@
 __author__ = 'XuJunjie'
 
 import math
-from pylab import *
-
-
-def Binomial(n,i):
-	return math.factorial(n)/(math.factorial(n-i)*(math.factorial(i)))
+from pylab import *#pylab.pinv
+from scipy.misc import comb
 
 def BezierFunction(n):
-	B = []
-	for t in T:
-		B_t = []
-		for i in range(n+1):
-			B_t.append(Binomial(n,i)*(t**i)*((1-t)**(n-i)))
-		B.append(B_t)
+	B = [[comb(n,i)*(t**i)*((1-t)**(n-i)) for i in range(n+1)] for t in T]
 	return mat(B)
 
 def BezierCure(P):
 	B_C = BezierFunction(shape(P)[0]-1)*P
 	return B_C[:,0],B_C[:,1]
 
+
+
 if __name__ == '__main__':
 	T = np.linspace(0,1,100,endpoint = True)
-	P = [[5,0],[6,5],[10,-5],[12,-2]]
+	P = [[5,0],[6,5],[10,-5],[12,-2],[11,0]]
 	X,Y = BezierCure(mat(P))
 	plot(X,Y)
 	show()
